ad0a724cdb7ae6415776f2dac2e6f0a5de85fcf5
Jonathan Gruner <jogru0@gmail.com>
2024-04-04 14:34:00 +0200

    second impl

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 
 use anyhow::Error;
-use git2::{Repository, Sort};
+use git2::Repository;
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
 
     let repo = Repository::open(".")?;
 
-    let parent = repo.find_branch(&args[1], git2::BranchType::Local)?;
-    let parent = parent.get().peel(git2::ObjectType::Any)?.id();
+    let parent_branch = repo.find_branch(&args[1], git2::BranchType::Local)?;
+    let parent = parent_branch.get().peel(git2::ObjectType::Any)?.id();
 
     let branch = repo.head()?.peel(git2::ObjectType::Any)?.id();
 
@@ -24,31 +24,25 @@ fn main() -> Result<(), Error> {
     let merge_base_2 = repo.merge_base(branch, merge_base)?;
     assert_eq!(merge_base, merge_base_2);
 
-    let mut revwalk = repo.revwalk()?;
+    let parent = repo.reference_to_annotated_commit(parent_branch.get())?;
 
-    revwalk.push(branch)?;
-    revwalk.hide(parent)?;
+    let mut rebase = repo.rebase(None, Some(&parent), None, None)?;
 
-    revwalk.set_sorting(Sort::REVERSE)?;
+    while let Some(maybe_op) = rebase.next() {
+        let op = maybe_op?;
+        dbg!(op.kind().unwrap());
+        dbg!(op.id());
 
-    for rev in revwalk {
-        let oid = rev?;
-
-        let commit = repo.find_commit(oid)?;
+        let commit = repo.find_commit(op.id())?;
 
         let message = commit.message().unwrap();
 
         let new_message = format!("COST | {message}");
 
-        commit.amend(
-            None,
-            None,
-            Some(&repo.signature()?),
-            None,
-            Some(&new_message),
-            None,
-        )?;
+        rebase.commit(None, &repo.signature()?, Some(&new_message))?;
     }
+
+    rebase.finish(None)?;
 
     Ok(())
 }
